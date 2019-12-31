7bc2c1f602c0e578c682a77a11bca94cbdd7f13b
macken-hem <ken.kgaf@gmail.com>
2019-12-31 22:33:12 +0900

    up

---
+++ app/Http/Controllers/Postscontroller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class Postscontroller extends Controller
+{
+    public function index() {
+        $posts = Post::latest()->get();
+        return view('posts.index',['posts' => $posts]);
+    }
+
+    public function show(Post $post) {
+        return view('posts.show',['post' => $post]);
+
+    }
+}
