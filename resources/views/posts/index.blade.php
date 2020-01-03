97cc60147699b6b8756f10045b6e5f92a66392db
macken-hem <ken.kgaf@gmail.com>
2020-01-03 09:42:20 +0900

    up

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,23 +7,11 @@
     <a href = "{{ url('/posts/create') }}" class = "header-menu">NEW</a>
     Blog Posts
     </h1>
-    <!-- <ul>
-    @foreach ($posts as $post)
-      <li>
-      <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
-      <a href="{{ action('PostsController@edit', $post) }}">edit</a>
-      <a href="#" class = 'delete' data-id = "{{ $post->id }}" >delete</a>
-      <form method = "post" action = "{{ url('/posts', $post->id) }}" id = "form_{{ $post->id }}">
-      {{ csrf_field() }}
-      {{ method_field('delete') }}
-      </form>
-      </li>
-    @endforeach
-    </ul> -->
     <ul>
   @forelse ($posts as $post)
   <li>
   <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
+  <img src = "{{ $post->image }}">
     <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
     <a href="#" class="delete" data-id="{{ $post->id }}">[x]</a>
     <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
