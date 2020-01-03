eb81a8f5264e3dd77d9ff4860f951ba58a551e58
macken-hem <ken.kgaf@gmail.com>
2020-01-03 09:19:05 +0900

    up

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,9 +15,30 @@
   @forelse ($post->comments as $comment)
   <li>
   {{ $comment->body}}
+  <a href="#" class="delete" data-id="{{ $comment->id }}">[x]</a>
+    <form method="post" action="{{ action('CommentsController@destroy', [$post,$comment]) }}" id="form_{{ $comment->id }}">
+      {{ csrf_field() }}
+      {{ method_field('delete') }}
+    </form>
   </li>
   @empty
   <li>No comment yet</li>
   @endforelse
 </ul>
+
+<form method = "post" action = "{{ action('CommentsController@store',$post) }}">
+{{  csrf_field() }}
+
+<p>
+<input type = "text" name ="body" placeholder = "title" value = "{{ old('body') }}">
+@if ($errors->has('title'))
+<span class = "error">{{ $errors->first('title') }}</span>
+@endif
+</p>
+<p>
+<input type = "submit" value = "comment">
+</p>
+
+</form>
+<script src="{{ asset('/js/app.js') }}"></script>
 @endsection
