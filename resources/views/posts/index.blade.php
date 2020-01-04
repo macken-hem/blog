b9c8312b0cd316e96416518f51530b8ab05c621b
macken-hem <ken.kgaf@gmail.com>
2020-01-04 16:33:34 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,6 +12,7 @@
   <li>
   <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
   <p>{{ $post->image }}</p>
+  <img src="{{ asset('/public/img/'.$post->image) }}">
   <!-- <img src = "{{ $post->image }}"> -->
     <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
     <a href="#" class="delete" data-id="{{ $post->id }}">[x]</a>
