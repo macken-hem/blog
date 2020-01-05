9f63c65ef43e34adada6673e39526b3e58576625
macken-hem <ken.kgaf@gmail.com>
2020-01-05 13:50:19 +0900

    up

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,11 +4,13 @@
 
 @section('content')
 <div class = "create_title">
-<h1>
-新規投稿
-</h1>
-<!-- <a href = "{{ url('/') }}" class = "header-menu">戻る</a> -->
-</div>
+  <h1>
+  新規投稿
+  </h1>
+  <!-- <a href = "{{ url('/') }}" class = "header-menu">戻る</a> -->
+  </div>
+<div class = "create_field">
+<div class = "create_form">
 <form method = "post" action = "{{ url('/posts') }}" enctype = "multipart/form-data">
 {{  csrf_field() }}
 
@@ -34,8 +36,8 @@
 </p>
 
 <input type = "submit" value = "Add">
-
-
+</div>
+</div>
 
 </form>
 <script src="{{ asset('/js/app.js') }}"></script>
